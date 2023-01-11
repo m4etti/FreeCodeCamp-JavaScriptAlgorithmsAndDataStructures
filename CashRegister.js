776fbb7b8edcbf2ae126611c18ffd079a618520b
m4etti <matti.vout@gmail.com>
2023-01-11 11:17:08 +0200

    cash done

--- CashRegister.js
+++ CashRegister.js
@@ -1,38 +1,42 @@
 function checkCashRegister(price, cash, cid) {
-    const changeValues = [0.01, 0.05, 0.1, 0.25, 1, 5, 10, 20, 100];
+    const changeValues = [1, 5, 10, 25, 100, 500, 1000, 2000, 10000];   //values in cents to avoid float errors
+    let cid_now = [];   //new cid to calculate cid after change
+    for (const element of cid){
+        cid_now.push([element[0], element[1]*100]); //cid in cent to avoid float errors
+    }
     let totalCid = 0;
-    let changeAmount = cash - price;
+    let changeAmount = cash * 100 - price * 100;
     let change = [];
     let status = "OPEN";
     let stop = false;
-    for (let i in cid){
-        totalCid += cid[i][1];
+    for (let i in cid_now){
+        totalCid += cid_now[i][1];
     }
     if (changeAmount > totalCid){ 
         status = "INSUFFICIENT_FUNDS";
     }
     else{
-        while (changeAmount > 0 && !stop){
-            for (let i = cid.length-1; i>=0; i--){
-                if (cid[i][1] > 0 && changeAmount >= changeValues[i]){
-                    changeAmount -= changeValues[i];
-                    cid[i][1] -= changeValues[i];
+        while (changeAmount > 0 && !stop){  //loop until change is calculated or isn't possible
+            for (let i = cid_now.length-1; i>=0; i--){
+                if (cid_now[i][1] > 0 && changeAmount >= changeValues[i]){ //find largest possible money to give to change
+                    changeAmount -= changeValues[i] 
+                    cid_now[i][1] -= changeValues[i];
                     let index = -1;
-                    for (let x = 0; x < change.length; i++){
-                        if (change[x][0] == cid[i][0]){
+                    for (let x = 0; x < change.length; x++){    // is the note/coin already in change 
+                        if (change[x][0] == cid_now[i][0]){
                             index = x;
                             break;
                         }
                     }
                     if (index == -1){
-                        change.push([cid[i][0],changeValues[i]]);
+                        change.push([cid_now[i][0],changeValues[i]]);   //if not in change create new note/coin
                     }
                     else{
-                        change[index][1] += changeValues[i];
+                        change[index][1] += changeValues[i];    //if yeas increase amount
                     }                    
                     break;
                 }
-                if (i == 0){
+                if (i == 0){        // if i=0  and the while hasn't stopped change is not possible
                     status = "INSUFFICIENT_FUNDS";
                     change = [];
                     stop = true;
@@ -40,13 +44,24 @@ function checkCashRegister(price, cash, cid) {
             }
         }
     }
-    
-
-
+    totalCid = 0;
+    for (let i in cid_now){ //calculate cash in registry after change
+        totalCid += cid_now[i][1];
+    }
+    if (totalCid == 0){ // if no money left then CLOSED
+        status = "CLOSED";
+        change = cid;
+    }
+    else{
+        for (const element of change){
+            element[1] = element[1]/100;    //change back to dollars
+        }
+    } 
 
     return {status,change};
   }
 
 
 
- console.log(checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
+ console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
+console.log("jee")
